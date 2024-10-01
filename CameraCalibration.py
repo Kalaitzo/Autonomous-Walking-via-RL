a82a23bda36994138ce076163cdbac3a884bd0b2
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2024-10-01 20:13:41 +0300

    Camera calibration script

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -2,28 +2,33 @@ import numpy as np
 import cv2 as cv
 import glob
 
-# TODO: Capture the images from my camera and adjust the code to work accordingly to the chessboard images
-
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+checkerboard_size = (8, 6)  # Number of inner corners per a chessboard row and column
+square_size = 0.025  # Size of a square in meters (25 mm)
+
+# Prepare object points (0,0,0), (1,0,0), (2,0,0) ... for a 3D point cloud of the checkerboard
+objp = np.zeros((checkerboard_size[0] * checkerboard_size[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:checkerboard_size[0], 0:checkerboard_size[1]].T.reshape(-1, 2) * square_size
 
 # Arrays to store object points and image points from all the images.
 object_points = []  # 3d point in real world space
 image_points = []  # 2d points in image plane.
 
-images = glob.glob('img/calibration_images/*.jpg')
+images = glob.glob('img/calibration_images/*.png')
+
+gray_shape = None
 
 # show the images
 for file_name in images:
     img = cv.imread(file_name)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+    gray_shape = gray.shape[::-1]  # Get the shape of the image (Required for the calibration)
+
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+    ret, corners = cv.findChessboardCorners(gray, checkerboard_size, None)
 
     # If found, add object points, image points (after refining them)
     if ret:
@@ -33,8 +38,33 @@ for file_name in images:
         image_points.append(corners)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(2000)
+        cv.drawChessboardCorners(img, checkerboard_size, corners2, ret)
+        cv.imshow('Image', img)
+        cv.waitKey(100)
+
+        print("Image " + file_name + " processed")
 
 cv.destroyAllWindows()
+
+# Calibrate the camera
+ret, camera_matrix, distortion_coeffs, r_vectors, t_vectors = cv.calibrateCamera(object_points, image_points,
+                                                                                 gray_shape,
+                                                                                 None, None)
+
+print("Camera matrix: ")
+print(camera_matrix)
+
+print("Distortion coefficients: ")
+print(distortion_coeffs)
+
+# Save the camera matrix and distortion coefficients
+np.savez("camera_calibration_params.npz",
+         camera_matrix=camera_matrix, distortion_coefficients=distortion_coeffs)
+
+mean_error = 0
+for i in range(len(object_points)):
+    image_points2, _ = cv.projectPoints(object_points[i], r_vectors[i], t_vectors[i], camera_matrix, distortion_coeffs)
+    error = cv.norm(image_points[i], image_points2, cv.NORM_L2) / len(image_points2)
+    mean_error += error
+
+print("total error: {}".format(mean_error / len(object_points)))
