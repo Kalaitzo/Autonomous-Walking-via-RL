373185dbcb362257ab841c82ba16351b45477b5c
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2024-10-19 18:20:07 +0300

    Tested the camera movement measurements.

--- TestArucoDetectionCamera.py
+++ TestArucoDetectionCamera.py
@@ -11,27 +11,33 @@ directory = "img/aruco_markers/"  # Directory to save the image
 aruco_camera = ArucoDetectionCamera(marker_id=0, side_pixels=200, side_m=0.07,
                                     aruco_dict=aruco_dict_id, directory=directory)
 
+# if ket is the space key, then the initial position is set
 while True:
-    # Get the position of the marker
-    previous_position, previous_time = aruco_camera.getMarkerPositionAndTime()
+    initial_position, _ = aruco_camera.getMarkerPositionAndTime()
+    key = cv2.waitKey(1)  # Wait for a key press
+    if key == 32:  # If the key is the space key
+        break
+print("Initial position: ", initial_position)
 
-    time.sleep(1)  # This will be the time that it takes to apply the action on the robot (approx 1 second)
 
-    next_position, next_time = aruco_camera.getMarkerPositionAndTime()
+while True:
+    next_position, _ = aruco_camera.getMarkerPositionAndTime()
 
     # If the marker is not detected, do not calculate the velocity
     # This is only necessary for the beginning of the program.
     # Afterward, if the marker is not detected, the position stays the same as the previous one, and the velocity is 0
-    if previous_position is None or next_position is None:
-        velocity_x = 0
-        print("Marker not detected - Velocity of the marker: {:.2f} m/s".format(velocity_x))
+    if next_position is None:
+        print("Marker not detected - Displacement is 0")
         continue
 
-    # Get the velocity of the marker
-    velocity_x = aruco_camera.getMarkerVelocity(previous_position, previous_time, next_position, next_time)
-
     # Print the velocity of the marker with 2 decimal places
-    print("Velocity of the marker: {:.2f} m/s".format(velocity_x))
+    print("=====================================")
+    print("X-axis displacement: {:.2f} cm".format(100 * (next_position[0] - initial_position[0])))
+    print("Y-axis displacement: {:.2f} cm".format(100 * (next_position[1] - initial_position[1])))
+    print("Z-axis displacement: {:.2f} cm".format(100 * (next_position[2] - initial_position[2])))
+    print("=====================================\n")
+
+    time.sleep(1)  # Sleep for 1 second
 
     key = cv2.waitKey(1)  # Wait for a key press
     if key == 27:  # If the key is the escape key
