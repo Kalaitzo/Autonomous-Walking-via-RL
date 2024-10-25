d240292d89e92e2c2aaa225000a34231e9a1b272
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2024-10-25 19:05:21 +0300

    Updated attempt and training cycles

--- TrainRobot.py
+++ TrainRobot.py
@@ -6,10 +6,10 @@ from RealEnvironment import RealEnvironment
 from ArucoDetectionCamera import ArucoDetectionCamera
 
 key = 0  # The key to be pressed
-attempt = 2  # The amount of learning attempts
+attempt = 4  # The number of learning attempts
 time_steps = 500  # Number of steps to take in each training cycle
-training_cycles = 3  # Number of games to play
-training_cycle = 16  # The number of the training cycle that is about to start
+training_cycles = 3  # Number of training cycles to perform
+training_cycle = 1  # The number of the training cycle that is about to start
 load_time_steps = (training_cycle - 1) * time_steps  # Amount of time-steps the saved model has been trained for
 
 models_dir = "models/"  # The models directory
@@ -28,12 +28,12 @@ aruco_camera = ArucoDetectionCamera(marker_id=0, side_pixels=200, side_m=0.07,
 # Create the necessary gym type environment that interacts with the robot
 real_env = RealEnvironment(robot, aruco_camera, max_actions=100)
 
-load_model = True  # Whether to load a model or not
+load_model = False  # Whether to load a model or not
 # Create the learning model (SAC)
 if load_model:
     model = SAC.load(load_path, env=real_env)
 else:
-    model = SAC("MlpPolicy", real_env, batch_size=50, verbose=1, learning_starts=50)
+    model = SAC("MlpPolicy", real_env, batch_size=100, verbose=1, learning_starts=100)
 
 for i in range(training_cycles):
     # Before beginning the episode, check what the camera sees
@@ -52,7 +52,7 @@ for i in range(training_cycles):
     # - The action is applied to the robot by the RealEnvironment's step method
     # - The step method generates the robot_new_state, reward, done
     # - The model saves the new the returned values
-    # - The model learns if the amount of time-steps is larger than the learning starts value
+    # - The model learns if the number of time-steps is larger than the learning starts value
     model.learn(total_timesteps=time_steps, reset_num_timesteps=False)
 
     model_file = f"sac_robot{(i + training_cycle) * time_steps}"
