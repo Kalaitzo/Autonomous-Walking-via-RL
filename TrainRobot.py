5bc81b36f1bdc4a8a4dfde41a3197a785283cf34
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2024-10-28 14:15:03 +0200

    Updated training cycle number for loading.

--- TrainRobot.py
+++ TrainRobot.py
@@ -9,7 +9,7 @@ key = 0  # The key to be pressed
 attempt = 5  # The number of learning attempts
 time_steps = 500  # Number of steps to take in each training cycle
 training_cycles = 3  # Number of training cycles to perform
-training_cycle = 9  # The number of the training cycle that is about to start
+training_cycle = 13  # The number of the training cycle that is about to start
 load_time_steps = (training_cycle - 1) * time_steps  # Amount of time-steps the saved model has been trained for
 
 models_dir = "models/"  # The models directory
