c1045ad99eeee2824b0cad519b0cc99fa04e864c
Bill Kalaitzo <95544934+Kalaitzo@users.noreply.github.com>
2024-10-30 12:14:48 +0200

    Added last position saving and updated the loading variables.

--- TrainRobot.py
+++ TrainRobot.py
@@ -1,20 +1,21 @@
 import cv2
-from stable_baselines3 import SAC
-from utils import plot_learning_curve
+from utils import plot_learning_curve, plot_final_positions
 from RobotInterface import RobotInterface
 from RealEnvironment import RealEnvironment
+from stable_baselines3 import SAC
 from ArucoDetectionCamera import ArucoDetectionCamera
 
 key = 0  # The key to be pressed
 attempt = 5  # The number of learning attempts
 time_steps = 500  # Number of steps to take in each training cycle
 training_cycles = 3  # Number of training cycles to perform
-training_cycle = 13  # The number of the training cycle that is about to start
+training_cycle = 18  # The number of the training cycle that is about to start
 load_time_steps = (training_cycle - 1) * time_steps  # Amount of time-steps the saved model has been trained for
 
 models_dir = "models/"  # The models directory
 plots_dir = "plots/"  # The plots directory
 attempt_dir = f"attempt_{attempt}/"  # The attempt directory
+positions_dir = "positions/"  # The positions directory
 model_checkpoint = f"sac_robot{load_time_steps}"  # The model checkpoint
 load_path = models_dir + attempt_dir + model_checkpoint  # The complete path for the model loading
 
@@ -62,12 +63,20 @@ for i in range(training_cycles):
 
     # Get the scores from the environment
     scores = real_env.scores
+    # Get the final positions from the environment
+    final_positions = real_env.final_positions
 
     # Plot the scores
-    filename = f"Robot_Scores_{(i + training_cycle) * time_steps}.png"
-    figure_file = plots_dir + attempt_dir + filename
-    x = [episode + 1 for episode in range(len(scores))]
-    plot_learning_curve(x, scores, figure_file)
+    if i == 2:
+        learning_filename = f"Robot_Scores_{(i + training_cycle) * time_steps}.png"
+        learning_figure_file = plots_dir + attempt_dir + learning_filename
+        x = [episode + 1 for episode in range(len(scores))]
+        plot_learning_curve(x, scores, learning_figure_file)
+
+        # Plot the final positions
+        positions_filename = f"Robot_Final_Positions_{(i + training_cycle) * time_steps}.png"
+        positions_figure_file = plots_dir + attempt_dir + positions_dir + positions_filename
+        plot_final_positions(x, final_positions, positions_figure_file)
 
     # Reset the key
     key = 0
